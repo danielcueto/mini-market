353cdb15a6c5083d2040a26b100520a733e2084a
Daniel Cueto <danielcuetorrico@gmail.com>
2025-06-09 00:25:25 -0400

    order edit

--- src/pages/admin/OrderManagement.tsx
+++ src/pages/admin/OrderManagement.tsx
@@ -6,7 +6,7 @@ export function OrderManagement() {
 
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-      {" "}
+      
       <div className="mb-8">
         <h1 className="text-2xl font-bold text-gray-900 dark:text-white">
           Gestión de <span className="text-[#C6FF00]">Pedidos</span>
