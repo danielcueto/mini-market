2aec5460d6cba1276e674d665f6879803e3ec41a
daniel-032 <daniel.maldonado@assuresoft.tech>
2025-06-09 11:43:49 -0400

    feat: add in checkout order data

--- src/hooks/useOrders.ts
+++ src/hooks/useOrders.ts
@@ -3,18 +3,20 @@ import { type AppDispatch } from '../redux/store';
 import {
   addOrder,
   updateAllOrders,
-  selectOrders,
-  selectOrderById,
+  selectOrders
 } from '../redux/slices/ordersSlice';
 import type { Order } from '../interfaces/Order';
 
 export const useOrders = () => {
   const dispatch = useDispatch<AppDispatch>();
   const orders = useSelector(selectOrders);
+  const getOrder = (orderId: string): Order | undefined => {
+    return orders.find(order => order.id === orderId);
+  };
 
   return {
     orders,
-    getOrder: (id: string) => useSelector(selectOrderById(id)),
+    getOrder,
     addOrder: (order: Order) => dispatch(addOrder(order)),
     updateAllOrders: (newOrders: Order[]) => dispatch(updateAllOrders(newOrders)),
   };
